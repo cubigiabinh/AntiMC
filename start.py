93fb087981056245a3929f508eeb0bfabfd4bd97
Cubi Giabinh <cubigiabinh@gmail.com>
2023-05-08 17:17:19 +0700

    a little update

---
+++ start.py
@@ -0,0 +1,18 @@
+import os
+
+print("cubig's project - github.com/cubigiabinh")
+print(
+    "----- WARNING: Because Minecraft (or any 3rd party clients) use Java (or OpenJDK,...) so this program will block the any files name javaw.exe. We sorry for the inconvinience if this prevents any programs which require Java."
+    )
+print("________")
+
+user_choice = input("Do you want to continue?")
+
+if user_choice.lower() == 'yes':
+    print("You have been warned! Continuing...")
+    os.system("program.cmd")
+    exit()
+elif user_choice.lower() == 'no':
+    exit()
+else:
+    exit()
